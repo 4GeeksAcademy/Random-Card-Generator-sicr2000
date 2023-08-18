afcd57bf2b87c6a54f0f630807e5ac9da5412b1d
bynx <138079781+sicr2000@users.noreply.github.com>
2023-08-18 03:04:42 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+  function randomSymbolFunction() {
+    const randomSymbol = symbols[Math.floor(Math.random() * symbols.lenght)];
+
+    return symbols[randomSymbol];
+  }
+
+  function randomNumberFunction() {
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.lenght)];
+
+    return numbers[randomNumber];
+  }
+
+  console.log(randomNumberFunction());
+  console.log(randomSymbolFunction());
+
+  document.querySelector(".icon").innerHTML = randomSymbolFunction();
+  document.querySelector(".number").innerHTML = randomNumberFunction();
 };
